65f7087e1865a73d6c34f4c995a53f2acc303adb
jcbgrub <jacob.hp.grub@gmail.com>
2020-07-27 13:51:37 +0000

    added the email validator and flaksbcrypt

--- flask-app/application/__init__.py
+++ flask-app/application/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
@@ -8,5 +9,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
 app.config['SECRET_KEY'] = str(os.getenv('SECRET_KEY'))
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
 
 from application import routes
