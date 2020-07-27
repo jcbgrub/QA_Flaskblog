f28e195e24698cb1b886a3aa557e3c7109a9d020
jcbgrub <jacob.hp.grub@gmail.com>
2020-07-27 14:56:11 +0000

    add login page

--- flask-app/application/__init__.py
+++ flask-app/application/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 import os
 
 app = Flask(__name__)
@@ -11,5 +12,7 @@ app.config['SECRET_KEY'] = str(os.getenv('SECRET_KEY'))
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 from application import routes
