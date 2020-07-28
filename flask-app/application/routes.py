283ff15f6bbc7e7e45366bb60ee2e7e8d842c9f6
jcbgrub <jacob.hp.grub@gmail.com>
2020-07-28 11:26:09 +0000

    updated account page

--- flask-app/application/routes.py
+++ flask-app/application/routes.py
@@ -4,7 +4,7 @@ from flask import render_template, redirect, url_for, request
 from application import app, db, bcrypt
 # define routes for / & /home, this function will be called when these are accessed
 from application.models import Posts, Users
-from application.forms import PostForm, RegistrationForm, LoginForm
+from application.forms import PostForm, RegistrationForm, LoginForm, UpdateAccountForm
 from flask_login import login_user, current_user, logout_user, login_required
 
 
@@ -18,7 +18,7 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
@@ -36,15 +36,20 @@ def login():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hash_pw = bcrypt.generate_password_hash(form.password.data)
 
-        user = Users(email=form.email.data, password=hash_pw)
+        user = Users(
+        first_name=form.first_name.data,
+        last_name=form.last_name.data,
+        email=form.email.data,
+        password=hash_pw)
 
         db.session.add(user)
         db.session.commit()
-
         return redirect(url_for('post'))
     return render_template('register.html', title='Register', form=form)
 
@@ -54,11 +59,9 @@ def post():
     form = PostForm()
     if form.validate_on_submit():
         postData = Posts(
-            first_name = form.first_name.data,
-            last_name = form.last_name.data,
             title = form.title.data,
-            content = form.content.data
-        )
+            content = form.content.data,
+            author = current_user)
 
         db.session.add(postData)
         db.session.commit()
@@ -74,3 +77,19 @@ def post():
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
+@app.route('/account', methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.first_name = form.first_name.data
+        current_user.last_name = form.last_name.data
+        current_user.email = form.email.data
+        db.session.commit()
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.first_name.data = current_user.first_name
+        form.last_name.data = current_user.last_name
+        form.email.data = current_user.email
+    return render_template('account.html',name=current_user.first_name, title='Account', form=form)
