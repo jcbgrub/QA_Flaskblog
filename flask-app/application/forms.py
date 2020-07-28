45a7f8ba2eaf6ca1b1150f687fba083cd5e6811b
“jcbgrub” <jacob.hp.grub@gmail.com>
2020-07-28 12:52:00 +0100

    new updates on posts

--- flask-app/application/forms.py
+++ flask-app/application/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from application.models import Users
+from application.models import Users, Posts
 from flask_login import current_user
 
 class PostForm(FlaskForm):
